1ffaa6ec4847dccb5dbbe58265866361419c663f
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2024-03-28 02:55:03 +0300

    Time: 46 ms (80.58%) | Memory: 16.8 MB (56.53%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -4,8 +4,6 @@ class Solution:
             if nums[i] == nums[i+1]:
                 nums[i] = nums[i] *2
                 nums[i+1] = 0
-        for i in nums:
-            if i == 0:
-                nums.remove(0)
-                nums.append(0)
-        return nums
+        cou = nums.count(0)
+        nums = [i for i in nums if i != 0]
+        return nums + ([0]*cou)
