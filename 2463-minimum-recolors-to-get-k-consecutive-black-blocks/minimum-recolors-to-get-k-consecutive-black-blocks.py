a150981a60f4a95a7c55e1a2f4317de2068183d9
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2024-04-12 18:08:40 +0300

    Time: 40 ms (32.31%) | Memory: 16.6 MB (65.12%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -3,11 +3,9 @@ class Solution:
      
         l = 0
         minop = blocks[:k].count("W")
-        print(minop)
-        print(len(blocks))
         for i in range(k,len(blocks)):
             coun = Counter(blocks[l:i])
-            print(coun)
+            
             l += 1
             minop = min(minop, coun["W"])
         minop = min(minop,Counter(blocks[l:len(blocks)+1])['W'])
