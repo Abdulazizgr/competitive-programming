3490cf88aa5a466d1b28478009f2883a8d18d7ff
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2024-11-23 14:57:22 +0300

    Time: 587 ms (5.43%) | Memory: 17.1 MB (64.94%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,5 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        pep = list(zip(names, heights))
-        pep.sort(key=lambda x: x[1], reverse=True)
-        return [i for i, _ in pep]
+        pep = list(zip(names,heights))
+    
+        n = len(pep)
+  
+
+        for i in range(n):
+            for j in range(n - i - 1):
+                if pep[j][1] < pep[j + 1][1]:  
+                    pep[j], pep[j + 1] = pep[j + 1], pep[j]
+        
+ 
+        return [name for name, _ in pep]
+
+
