42ac9e8a4d5b68f82c9a5323ca446a03c6e001e5
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2024-12-06 14:28:19 +0300

    Time: 0 ms (100.00%) | Memory: 17.3 MB (7.54%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -2,5 +2,30 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
 
         s = s.split()
+        p = list(pattern)
+        print(s,p)
+        dic = {}
+        dics ={}
+
+        if len(s) == len(p):
+            for i in range(len(p)):
+                if p[i] in dic:
+                    if dic[p[i]] != s[i]:
+                        return False
+                else:
+                    dic[p[i]] = s[i]
+            for i in range(len(p)):
+                if s[i] in dics:
+                    if dics[s[i]] != p[i]:
+                        return False
+                else:
+                    dics[s[i]] = p[i]
+        else:
+            return False
+        return True
+                    
+               
+        print(dic)
+       
       
-        return (len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern,s))))
+        # return (len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern,s))))
