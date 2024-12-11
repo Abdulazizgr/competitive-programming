a4294b383aac384a7e0445e75c38537677b9a6ff
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2024-12-11 21:42:54 +0300

    Time: 7 ms (9.05%) | Memory: 17.6 MB (10.83%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfArithmeticSlices(self, A: List[int]) -> int:
+        le=len(A)
+        l=[0]*(le)
+        for i in range(2,le):
+            if A[i]-A[i-1] == A[i-1]-A[i-2]:
+                l[i]=1+l[i-1]
+                print(l)
+        return sum(l)
