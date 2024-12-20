153df003f43ef75eb7151c8b52715bfecc1444c7
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2024-12-20 23:31:50 +0300

    Time: 37 ms (44.72%) | Memory: 35.7 MB (6.62%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,7 +1,7 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         le = []
-        eq = []
+       
         gr = []
     
 
@@ -10,6 +10,5 @@ class Solution:
                gr.append(nums[i])
             elif nums[i] < pivot:
                 le.append(nums[i])
-            else:
-                eq.append(nums[i])
-        return le + eq + gr
+        n = len(nums) - len(le) - len(gr)
+        return le + [pivot]*n + gr
