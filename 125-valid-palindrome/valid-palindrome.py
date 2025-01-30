34027f17098fa9d84b9dd9983a7945884f466135
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-01-30 20:40:56 +0300

    Time: 10 ms (41.27%) | Memory: 18.3 MB (42.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,9 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        an = []
+        ans = ""
         for i in range(len(s)):
-            if s[i].isalnum():
-                an.append(s[i])
-
-        return an == an[::-1]
+            if ord(s[i]) >= 97 and ord(s[i]) <= 122 or ord(s[i]) >= 48 and ord(s[i]) <= 57 :
+                ans += s[i]
+        print(ans)
+    
+        return ans == ans[::-1]
