22458ec9119d153d338aea11d22dc58b1fd94b6b
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-02-04 08:30:39 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.87%) - LeetSync

--- 858-masking-personal-information/masking-personal-information.py
+++ 858-masking-personal-information/masking-personal-information.py
@@ -6,19 +6,14 @@ class Solution:
             print(name)
             return name[0][:1] +"*****"+ name[0][-1:] +"@"+ name[1]
         else:
-            nums =[]
+            nums = ''
             for i in range(len(s)):
                 if s[i].isdigit():
-                    nums.append(s[i])
+                    nums+= s[i]
             if len(nums) == 10:
-                return "***-***-" + "".join(nums[-4:])
-            elif len(nums) == 11:
-                return "+*-***-***-" + "".join(nums[-4:])
-            elif len(nums) == 12:
-                return "+**-***-***-" + "".join(nums[-4:])
-            elif len(nums) == 13:
-                return "+***-***-***-" + "".join(nums[-4:])
-
+                return "***-***-" + nums[-4:]
+            else:
+                return '+' + '*'* (len(nums)-10) + "-***-***-" + nums[-4:]
 
 
                 
