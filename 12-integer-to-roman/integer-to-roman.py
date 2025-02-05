6215a0d86bfaaf5115f6fca07d8f10b89f2e9494
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-02-05 16:06:15 +0300

    Time: 9 ms (25.27%) | Memory: 17.9 MB (38.56%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman_nums = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], 
+        [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]
+]       
+        ans = ""
+        for value  in roman_nums:
+            if num // value[0]:
+                count = num // value[0]
+                ans += value[1] *count
+                num = num % value[0]
+        return ans 
+
+                
