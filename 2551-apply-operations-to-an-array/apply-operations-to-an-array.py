bf39412fc96799453ed330d75e678915615df431
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-02-06 11:21:55 +0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (5.41%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,9 +1,14 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1]:
-                nums[i] = nums[i] *2
-                nums[i+1] = 0
-        cou = nums.count(0)
-        nums = [i for i in nums if i != 0]
-        return nums + ([0]*cou)
+        ans = []
+        for index in range(len(nums)-1):
+            if nums[index] == nums[index+1]:
+                nums[index] *= 2
+                nums[index + 1] = 0
+        zeros = nums.count(0)
+        for val in nums:
+            if val != 0:
+                ans.append(val)
+        for i in range(zeros):
+            ans.append(0)
+        return ans
