0efdaf98e43c85c0f45a115065a8aa459cc4b9d0
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-02-08 22:48:28 +0300

    Time: 223 ms (18.99%) | Memory: 18.6 MB (69.16%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -1,24 +1,25 @@
-from typing import List
-
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
         rows, cols = len(img), len(img[0])
-        result = [[0] * cols for _ in range(rows)] 
-        def isvalid(r,c):
-            return 0 <= r < len(img) and 0 <= c < len(img[0]) 
+        smoothed_img = [[0] * cols for _ in range(rows)] 
+
+        def is_valid(row, col):
+            return 0 <= row < rows and 0 <= col < cols  
+
         for row in range(rows):
             for col in range(cols):
-                surrounding = [
-                    [row-1,col],[row+1,col],[row,col-1],
-                    [row,col+1],[row-1,col-1],[row+1,col+1],
-                    [row-1,col+1],[row+1,col-1]
+                neighbors = [
+                    (row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1),
+                    (row - 1, col - 1), (row + 1, col + 1), (row - 1, col + 1), (row + 1, col - 1)
                 ]
-                count = 1
-                sum_n = img[row][col]
-                for r,c in surrounding:
-                    if isvalid(r,c):
-                        count += 1
-                        sum_n += img[r][c]
+                pixel_sum = img[row][col]
+                pixel_count = 1
 
-                result[row][col] = sum_n // count  
-        return result  
+                for r, c in neighbors:
+                    if is_valid(r, c):
+                        pixel_sum += img[r][c]
+                        pixel_count += 1
+
+                smoothed_img[row][col] = pixel_sum // pixel_count  
+
+        return smoothed_img  
