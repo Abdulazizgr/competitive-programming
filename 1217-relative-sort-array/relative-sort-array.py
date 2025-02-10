cf4cf1fb08223ce404c97a51b20f74be25c0261d
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-02-10 15:00:17 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.00%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,12 +1,10 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        ans = []
-        for i in range(len(arr2)):
-            n = arr1.count(arr2[i])
-            for j in range(n):
-                ans.append(arr2[i])
-                print(ans)
-                arr1.remove(arr2[i])
-                print(arr1)
-        arr1.sort()
-        return (ans+arr1)
+        result = []
+        freq = Counter(arr1)
+
+        for num in arr2:
+            result.extend([num] * freq.pop(num, 0))
+
+        result.extend(sorted(freq.elements()))
+        return result
