185ef21b494f7c49d1d5f8c0141fff5027475a15
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-02-10 23:20:16 +0300

    Time: 23 ms (5.08%) | Memory: 17.7 MB (79.48%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,12 +1,21 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        if len(nums) == nums.count(0):
-            return str(nums[0])
-        nums = [str(num) for num in nums]
-        nums.sort(key=lambda x: x * 10, reverse= True)
-        # srt = srt[::-1]
-        print(nums)
-        res=""
-        for i in nums:
-            res += str(i)
-        return res 
+        n = len(nums)
+        if nums.count(0) == n:
+            return "0"
+        for i in range(n):
+            nums[i] = str(nums[i])
+    
+        for i in range(n):
+            for j in range(i + 1, n):
+                if int(nums[i] + nums[j]) < int(nums[j] + nums[i]):
+                    nums[i], nums[j] = nums[j], nums[i]
+         
+        return "".join(nums)
+
+
+
+
+
+
+        
