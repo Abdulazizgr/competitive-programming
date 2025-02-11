7c3574fcefa4558f01ba3e6d8c18266fe1c090c0
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-02-11 14:12:00 +0300

    Time: 0 ms (100.00%) | Memory: 22.7 MB (48.70%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -1,15 +1,6 @@
 class Solution:
     def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
-        n = len(score)
-        dic = {}
-        for i in range(n):
-            dic[score[i][k]] = score[i]
-        ans = dict (sorted(dic.items(),reverse=True))
-        value = list(ans.values())
-        
-        
-       
+        score.sort(key = lambda x:x[k],reverse = True)
 
 
-
-        return value
+        return score
