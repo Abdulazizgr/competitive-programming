1e31680593a197764e96239e3bf2499814f6f2d9
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-02-13 15:58:39 +0300

    Time: 55 ms (33.81%) | Memory: 17.9 MB (59.22%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,43 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1c = Counter(s1)
-        n = len(s1)
-        # print(Counter(s2[3:5]) == Counter(s1))
-        print(Counter(s2[1:4]) == Counter(s1))
+        left = 0
+        if len(s1) > len(s2):
+            return False
+        freq_s1 = Counter(s1)
+        freq_s2 = Counter(s2[:len(s1)-1])
 
-        for i in range(len(s2)-n+1):
-            s2c = Counter(s2[i:i+n])
-            print(s2c,len(s2)-n)
-            if s1c == s2c:
+        for right in range(len(s1)-1,len(s2)):
+            freq_s2[s2[right]] += 1
+
+            if freq_s1 == freq_s2:
                 return True
+            freq_s2[s2[left]] -= 1
+            if freq_s2[s2[left]] == 0:
+                del freq_s2[s2[left]]
+            left += 1
+
         return False
 
-      
-      
-
-
-
-
-        # l = 0
-        # r  =0
-        # count = 0
-
-        # s1 = s1[::-1]
-        # print(s1)
-
-        # while l < len(s1) and r < len(s2):
-        #     if s1[l] == s2[r]:
-        #         print(s2[l:len(s1)],s1[:])
-        #         if s1[:] == s2[l:len(s1)]:
-        #             return True
-             
-                
-        #         r += 1
-        #     else:
-        #         r += 1
-      
-       
-        # return False
-
-
-      
