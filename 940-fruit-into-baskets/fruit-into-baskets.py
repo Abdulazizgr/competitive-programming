8af074e0a5f4f040f374a649ea0adefd2641592b
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-02-14 17:52:40 +0300

    Time: 245 ms (14.60%) | Memory: 23.6 MB (43.67%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        freq = defaultdict(int)
+        left = 0
+        max_sum = 0
+
+        for right in range(len(fruits)):
+            freq[fruits[right]] += 1
+            while len(freq) > 2:
+                freq[fruits[left]] -= 1
+                if freq[fruits[left]] == 0:
+                    del freq[fruits[left]]
+                left += 1
+            max_sum = max(max_sum ,sum(freq.values()))
+        return max_sum
