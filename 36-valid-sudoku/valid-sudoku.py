43a921a893e301934d6d6c8945a31892a9947e0f
Abdulaziz21 <136096645+Abdulazizgr@users.noreply.github.com>
2025-07-03 22:22:36 +0300

    Time: 4 ms (58.68%) | Memory: 17.8 MB (43.71%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,45 +1,45 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def get_neighbors(row, col):
-            return [
-                (row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1),
-                (row - 1, col - 1), (row + 1, col + 1), (row - 1, col + 1), (row + 1, col -1)
-            ]  
+        # rule 1 and rule 2
+
         for row in range(9):
             horizontal = set()
             vertical = set()
             for col in range(9):
                 if board[row][col].isdigit():
-                    if board[row][col] not in horizontal :
+                    if board[row][col] not in horizontal:
                         horizontal.add(board[row][col])
                     else:
                         return False
                 if board[col][row].isdigit():
-                    if board[col][row] not in vertical :
+                    if board[col][row] not in vertical:
                         vertical.add(board[col][row])
                     else:
                         return False
-        
-        middle_points = [
-        (1,1), (1,4), (1,7),
-        (4,1), (4,4), (4,7),
-        (7,1), (7,4), (7,7) ]
 
+        # rule 3
+        middle_points = [
+            (1,1),(1,4),(1,7),
+            (4,1),(4,4),(4,7),
+            (7,1),(7,4),(7,7)
+        ]
+
+        def get_neighbors(row,col):
+            return [
+                (row-1,col-1),(row-1,col),(row-1,col+1),
+                (row,col+1),(row+1,col+1),(row+1,col),
+                (row+1,col-1),(row,col-1)
+            ]
         for row,col in middle_points:
             sub_box = set()
             if board[row][col].isdigit():
                 sub_box.add(board[row][col])
-            neighbors = get_neighbors(row, col)
-            for r, c in neighbors:
-                if board[r][c].isdigit():
-                    if board[r][c] not in sub_box :
-                        sub_box.add(board[r][c])
+            negihborbs = get_neighbors(row,col)
+            for ro ,co in negihborbs:
+                if board[ro][co].isdigit():
+                    if board[ro][co] not in sub_box:
+                        sub_box.add( board[ro][co])
                     else:
                         return False
-
-        
-        return True
-        
-
-
-        
+        return True 
+                    
